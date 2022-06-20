4489b55c6596a00935ba7df1628b1beef8351c01
Leandro724 <88081194+Leandro724@users.noreply.github.com>
2022-06-20 10:08:47 +0200

    Doc type change
    Changed the document type to PHP in order to add functionality and be able to store user data

--- login.php
+++ login.php
@@ -4,19 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SignUp</title>
+    <title>Login</title>
     <link rel="stylesheet" href="stylesheet.css">
 </head>
 <body>
-    <form method="post" action="#">
-        <p>Sign Up below and save loads on your next accomodation!!!</p>
-        <input class="inputField" type="text" name="name" placeholder="First Name" required/><br><br>
-        <input class="inputField" type="text" name="surname" placeholder="Surname" required/><br><br>
+    <form>
+        <p>Welcome back Leandro, login and start saving :)</p>
         <input class="inputField" type="text" name="email" placeholder="Email Address" required/><br><br>
         <input class="inputField" type="password" name="password" placeholder="Password" required/><br><br>
-        <input class="button" type="submit" value="Register"/><br><br>
-        <p>Already have an account? <a href="login.html">Login Here.</a></p><br><br>
+        <input class="button" type="submit" value="Login"/><br>
+        <p>Don't have an account? <a href="SignUp.php">Register Here.</a></p><br><br>
     </form>
-    
+   
 </body>
 </html>
