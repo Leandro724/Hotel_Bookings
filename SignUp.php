0aa61e96ff64a4528990d0267973003b3a6b8c57
Leandro724 <88081194+Leandro724@users.noreply.github.com>
2022-06-21 10:53:45 +0200

    SignUp Page
    I modified my SignUp page to be able to extract the data entered by a user and input them in a JSON file as my form of database

--- SignUp.php
+++ SignUp.php
@@ -1,3 +1,13 @@
+<!-- Rquire a SignUp class for new users once the form is submitted -->
+<?php require("newUser.php")?>
+
+<!-- Once the form is submitted the following data will be captured -->
+<?php 
+if(isset($_POST['submit'])){
+    $user = new SignUpUser($_POST['name'],$_POST['surname'],$_POST['email'],$_POST['password']);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,16 +15,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SignUp</title>
+    <!-- Linking in the stylesheet -->
     <link rel="stylesheet" href="stylesheet.css">
 </head>
 <body>
-    <form method="post" action="#">
+
+<!-- SignUp form for each user -->
+    <form method="post" action="">
         <p>Sign Up below and save loads on your next accomodation!!!</p>
         <input class="inputField" type="text" name="name" placeholder="First Name" required/><br><br>
         <input class="inputField" type="text" name="surname" placeholder="Surname" required/><br><br>
         <input class="inputField" type="text" name="email" placeholder="Email Address" required/><br><br>
         <input class="inputField" type="password" name="password" placeholder="Password" required/><br><br>
-        <input class="button" type="submit" value="Register"/><br><br>
+        <button class="button" type="submit" name="submit">Sign Up</button>
+        <p><?php echo @$user ->error?></p>
+        <p><?php echo @$user ->success?></p>
         <p>Already have an account? <a href="login.php">Login Here.</a></p><br><br>
     </form>
     
